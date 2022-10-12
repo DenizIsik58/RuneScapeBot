f1b9281c823359ebcd9123e1c2af71f70914fe75
Deniz Isik <90033193+DenizIsik58@users.noreply.github.com>
2022-10-12 07:50:29 -0700

    Remove new speed questing worlds

--- scripts/revs/src/scripts/rev/WorldManager.java
+++ scripts/revs/src/scripts/rev/WorldManager.java
@@ -22,7 +22,7 @@ public class WorldManager {
                         World.Type.PVP,
                         World.Type.DEADMAN,
                         World.Type.DEADMAN_TOURNAMENT)
-                .worldNumberNotEquals(403, 404, 407, 408, 411, 412)
+                .worldNumberNotEquals(403, 404, 407, 408, 411, 412, 502, 503, 540, 541, 549, 550, 568, 569, 576, 581)
                 .isNotCurrentWorld()
                 .findRandom()
                 .map(i ->  {
